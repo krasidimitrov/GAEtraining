8e69757513fd6c8488f7f32384c606415c80f7ec
Krasimir Dimitrov <kpackapgo@gmail.com>
2012-07-04 16:09:52 +0300

    added first test for the repository

---
+++ test/com/clouway/appengineexample/server/UserRepositoryImplTest.java
@@ -0,0 +1,56 @@
+package com.clouway.appengineexample.server;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.EntityNotFoundException;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
+import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Krasimir Dimitrov (kpackapgo@gmail.com, krasimir.dimitrov@clouway.com)
+ */
+public class UserRepositoryImplTest {
+
+
+  DatastoreService datastoreService = DatastoreServiceFactory.getDatastoreService();
+  UserRepository userRepository = new UserRepositoryImpl(datastoreService);
+
+  private final LocalServiceTestHelper helper =
+          new LocalServiceTestHelper(new LocalDatastoreServiceTestConfig().setDefaultHighRepJobPolicyUnappliedJobPercentage(100));
+
+  @Before
+  public void setUp() {
+    helper.setUp();
+  }
+
+  @After
+  public void tearDown() {
+    helper.tearDown();
+  }
+
+
+  @Test
+  public void addEntityToDataStore() throws EntityNotFoundException {
+
+    Entity entity = new Entity("User");
+    entity.setProperty("username", "kpackapgo");
+    entity.setProperty("password", "abc123");
+    entity.setProperty("email", "555@mail.bg");
+    
+    userRepository.add(entity);
+    Key key = entity.getKey();
+//    Key key2 = KeyFactory.createKey("User",)
+//    datastoreService.get(key);
+    assertThat(entity, is(equalTo(datastoreService.get(key))));
+  }
+
+}
